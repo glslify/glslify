f6ccadd76348bac7797a1bf16f72e7ee36ef1559
Hugh Kennedy <hughskennedy@gmail.com>
2016-07-11 20:21:27 +1000

    try and fix cli test on windows

--- test/cli.js
+++ test/cli.js
@@ -1,11 +1,12 @@
 const exec    = require('child_process').exec
 const spawn   = require('child_process').spawn
 const quote   = require('shell-quote').quote
+const slash   = require('slash')
 const test    = require('tape')
 const path    = require('path')
 const fs      = require('fs')
 const bl      = require('bl')
-const glslify = require.resolve('../bin')
+const glslify = slash(require.resolve('../bin'))
 
 test('cli: globe.frag', function(t) {
   exec('node '+quote([ glslify,
